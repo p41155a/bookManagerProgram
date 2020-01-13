b7c18b99bf6dcaf320bb690324b98abd7b72f99c
p41155a <50395024+p41155a@users.noreply.github.com>
2020-01-13 13:20:31 +0900

    Initial Commit

---
+++ bookManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bookManager
+//
+//  Created by Yoojin Park on 2020/01/13.
+//  Copyright © 2020 Yoojin Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
